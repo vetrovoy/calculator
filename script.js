fac7d4cc0048740cf0371add3170e17135dbc74f
vetrovoy <v3trovoy@gmail.com>
2021-03-01 00:51:59 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const keys = document.querySelectorAll('.btn');
+const result = document.querySelector('.result-area');
+
+const backspace = document.querySelector('#backspace');
+const clear = document.querySelector('#clear');
+const equal = document.querySelector('#equal');
+const square = document.querySelector('#square');
+const cube = document.querySelector('#cube');
+
+keys.forEach((e) => {
+  const keyValue = e.getAttribute('value');
+
+  e.addEventListener('click', () => {
+    result.value = result.value.concat(keyValue);
+  });
+});
+
+// Delete
+backspace.addEventListener('click', () => {
+  result.value = result.value.slice(0, -1)
+});
+
+// Clear
+clear.addEventListener('click', () => {
+  result.value = '';
+});
+
+// Equal
+equal.addEventListener('click', () => {
+  if(!result.value == "") {
+    result.value = eval(result.value);
+  };
+});
+
+// Square
+square.addEventListener('click', () => {
+  if (!result.value == "") {
+    result.value = eval(result.value * result.value);
+  };
+});
+
+// Cube
+cube.addEventListener('click', () => {
+  if (!result.value == "") {
+    result.value = eval(result.value * result.value * result.value);
+  };
+
+//Check
+  if(isNaN(result.value)){
+    result.value = "";
+  }
+});
+
